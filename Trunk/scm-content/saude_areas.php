e2c7e0bc5328d13b519c4f08be4be513ca91447d
jodufra <joeld.fran@hotmail.com>
2014-06-15 01:11:15 +0100

    saude_areas

---
+++ Trunk/scm-content/saude_areas.php
@@ -0,0 +1,28 @@
+<?php 
+require_once 'controllers/commons.php'; 
+$title="Santa Casa da Misericórdia de Leiria" ;
+$keywords="";
+$description=""; 
+$body_onload=""; 
+$firstnavbar=0;
+$secondnavbar=array($firstnavbar,0);
+$area = 0;
+if(isset($_POST) && isset($_POST['area'])){
+    $area = $_POST['area'];
+}
+
+include_once VIEWS_DIR.'header_open.php';
+// INICIO todos os includes e scripts particulares da pagina
+echo "<link rel='stylesheet' href='".STYLES_DIR."slider.css' type='text/css'/>\n";     
+echo "<script type='text/javascript' src='".INCLUDES_DIR."jquery.cycle.all.js'></script>\n";
+echo "<script type='text/javascript' src='".INCLUDES_DIR."jquery.easing.1.3.js'></script>"; 
+echo "<script type='text/javascript' src='".JS_DIR."slider.js'></script>\n";  
+// END 
+include_once VIEWS_DIR.'header_close.php';
+include_once VIEWS_DIR.'top.php';
+
+
+//include_once 'views/home/index.php';
+
+
+include_once VIEWS_DIR.'footer.php';
