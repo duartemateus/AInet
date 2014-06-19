d563d8b7dfea1e865804b418e270e9c820db47b0
jodufra <joeld.fran@hotmail.com>
2014-06-19 04:19:12 +0100

    "no bugs" verssion 7

--- Testes/scml-project/public/pessoal_editar.php
+++ Testes/scml-project/public/pessoal_editar.php
@@ -2,17 +2,9 @@
 require_once '../config/commons.php';
 $vars = array();
 require_once './auth.php';
-if($auth->isAuthenticated()){
-    include VIEW_DIR.'utils/goback.php';
-    exit;
-}
 $controller = new controller_PessoalRegistar($vars);
-if($controller->prepare_vars($vars)){
-    include VIEW_DIR.'utils/goback.php';
-    exit;
-}
+$controller->prepare_vars($vars);
 extract($vars);
-$person = $pessoa[0];
 include VIEW_DIR.'head.php';
 include $controller->get_view($vars);
 include VIEW_DIR.'foot.php';
--- Testes/scml-project/public/pessoal_registar.php
+++ Testes/scml-project/public/pessoal_registar.php
@@ -2,15 +2,8 @@
 require_once '../config/commons.php';
 $vars = array();
 require_once './auth.php';
-if($auth->isAuthenticated()){
-    include VIEW_DIR.'utils/goback.php';
-    exit;
-}
-$controller = new controller_PessoalRegistar($vars);
-if($controller->prepare_vars($vars)){
-    include VIEW_DIR.'utils/goback.php';
-    exit;
-}
+$controller = new controller_SaudeRegistar($vars);
+$controller->prepare_vars($vars);
 extract($vars);
 include VIEW_DIR.'head.php';
 include $controller->get_view($vars);
